07c93c74989fe7cb6f8599a5d296b6141a611ece
Moeen Zamani <moeen.zamani@gmail.com>
2017-08-06 17:02:52 +0430

    created package setup.py module and added classifiers.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='pay-ir',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Moeen Zamani',
+    author_email='moeen.zamani@gmail.com',
+    license='MIT',
+    description='Python interface for pay.ir payment gateway.',
+    url='https://github.com/moeenz/pay-ir',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Project :: pay-ir'
+    ],
+)
